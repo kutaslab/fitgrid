8978a0da1165562ebc14ce6f2dfb15f36513fe04
Lauren <ldliao@mkgpu1.ucsd.edu>
2018-05-15 13:19:35 -0700

    added nose to tail go tests

--- eegr/tests/test_fit_bucket.py
+++ eegr/tests/test_fit_bucket.py
@@ -1,6 +1,7 @@
 from eegr import fit_bucket
 from eegr import sim_data as sd
 from random import randint
+import numpy as np 
 
 def fit_grid_gen():
     rand = randint(1,9)
@@ -15,12 +16,17 @@ def fit_grid_gen():
     RHS = "cat"
     fit_grids = fit_bucket.make_fit_table(df,LHS,RHS)
     
-    return rand, fit_grids
+    return rand, df, fit_grids
 
 def test_coef_shape():
-    rand, fit_grids = fit_grid_gen()
+    rand, df, fit_grids = fit_grid_gen()
     assert fit_grids[0]['fit']['coef'].shape[0] == rand
 
-def test_ci_val():
-    rand, fit_grids = fit_grid_gen()
-    assert(fit_grids[0]['fit']['ci'][0][0][0] == [0,0]).all()
+
+def test_ci():
+    rand, epochs, fit_grids = fit_grid_gen()
+    
+    times = epochs.index.levels[epochs.index.names.index('Time')].tolist()
+    values = (np.tile(times,1) + 0) * 2
+    for i, val in enumerate(values): 
+        assert(fit_grids[0]['fit']['ci'][i][0][0] == [val,val]).all()
--- eegr/tests/test_sim_data.py
+++ eegr/tests/test_sim_data.py
@@ -6,6 +6,7 @@ from statsmodels.formula.api import OLS
 from patsy import dmatrix
 from eegr import sim_data as sd
 import pytest
+from random import randint
 
 def df_test_set(complexity):
     if complexity == 'simple':
@@ -38,22 +39,22 @@ def df_test_set(complexity):
 
 def test_df_shape():
     simple_df = df_test_set('simple')
-    assert simple_df.shape == (2, 4)
+    assert simple_df.shape == (2, 3)
     cmplx1_df = df_test_set('complex1')
-    assert cmplx1_df.shape == (72, 5)
+    assert cmplx1_df.shape == (72, 4)
     cmplx2_df = df_test_set('complex2')
-    assert cmplx2_df.shape == (270, 5)
+    assert cmplx2_df.shape == (270, 4)
     
 def test_df_dtype():
     simple_df = df_test_set('simple')
-    assert type(simple_df.cat[0]) == str
-    assert type(simple_df.cat[1]) == str
+    assert type(simple_df.cat.values[0]) == str
+    assert type(simple_df.cat.values[1]) == str
     cmplx1_df = df_test_set('complex1')
-    assert type(cmplx1_df.cat[0]) == str
-    assert type(cmplx1_df.cat[len(cmplx1_df)-1]) == str
+    assert type(cmplx1_df.cat.values[0]) == str
+    assert type(cmplx1_df.cat.values[len(cmplx1_df)-1]) == str
     cmplx2_df = df_test_set('complex2')
-    assert type(cmplx2_df.cat[0]) == str
-    assert type(cmplx2_df.cat[len(cmplx1_df)-1]) == str
+    assert type(cmplx2_df.cat.values[0]) == str
+    assert type(cmplx2_df.cat.values[len(cmplx1_df)-1]) == str
 
 def test_empty_epoch00():
     with pytest.raises(Exception):
@@ -94,5 +95,53 @@ def test_time_stop_small():
         noise = (0,0)
         df = sd.df_gen(epoch_p=epoch_p, time_p=time_p, 
                    cat_p=cat_p, cont_p=cont_p, noise_p=noise)
-        
-      
+
+# Nose to tail test        
+# generate sample df for testing
+def sample_df():
+    rand = randint(1,4)
+    epoch_p = (rand,rand * rand+1)
+    time_p = (rand,2*rand,1)
+    cat_p = [(randint(1,4),rand)]
+    cont_p = [(rand,rand+5)]
+    noise = (0,0)
+    df = sd.df_gen(epoch_p=epoch_p, time_p=time_p, 
+                   cat_p=cat_p, cont_p=cont_p, noise_p=noise)
+    return epoch_p, time_p, cat_p, cont_p, df
+
+# test epoch idx
+def test_epoch_idx():
+    epoch_p, time_p, cat_p, cont_p, epochs = sample_df()
+    epoch_idx = epochs.index.levels[epochs.index.names.index('Epoch_idx')].tolist()
+    start, end = epoch_p
+    e = np.arange(start, end)
+    assert (epoch_idx == e).all
+
+# test time set
+def test_time_set():
+    epoch_p, time_p, cat_p, cont_p, epochs = sample_df()
+    times = epochs.index.levels[epochs.index.names.index('Time')].tolist()
+    start, end, step = time_p
+    t = np.arange(start, end, step)
+    assert (times == t).all
+
+# test if column matches cont_p range
+def test_cont_p():
+    epoch_p, time_p, cat_p, cont_p, epochs = sample_df()
+    for i,range_set in enumerate(cont_p):
+        start = range_set[0]
+        end = range_set[1]
+        col_label = 'cont_' + str(i+1) + '_range' + str(start) + ',' + str(end)
+        assert ((epochs[col_label].values > start) & (epochs[col_label].values < end)).all
+
+# test if data matches cat_p range
+def test_cat_p_match_data():
+    epoch_p, time_p, cat_p, cont_p, epochs = sample_df()
+    start, end = epoch_p
+    times = epochs.index.levels[epochs.index.names.index('Time')].tolist()
+    for cat in cat_p:
+        b0, b1 = cat[0], cat[1]
+        want_arr = (np.tile(times, end - start) + b0) * b1
+        label = 'L_' + str(b0) + '_' + str(b1)
+        act_arr = epochs[epochs['cat'] == label]['data'].values
+        assert (want_arr == act_arr).all
