8c9afc90b670988256a154d4201734ace5e60de3
Andrey Portnoy <aportnoy@ucsd.edu>
2018-08-24 13:14:35 -0700

    Attribution and requirements in setup.py.

--- setup.py
+++ setup.py
@@ -13,15 +13,14 @@ from setuptools import find_packages, setup
 setup(
     name='fitgrid',
     version='0.1',
-    description='regression ERP',
-    author='Tom Urbach, Lauren Liao, Andrey Portnoy',
-    author_email='turbach@ucsd.edu',
-    url='http://kutaslab.ucsd.edu/people/urbach',
+    description='rERP runner',
+    author='Andrey Portnoy',
+    author_email='aportnoy@ucsd.edu',
     packages=find_packages(),
     install_requires=[
-        'numpy',
-        'pandas',
         'patsy',
         'statsmodels',
+        'tqdm',
+        'pytables'
     ]
 )
