788110302e479aa5f704a6e1d58c73ae7321866b
Andrey Portnoy <aportnoy@ucsd.edu>
2018-08-30 16:42:14 -0700

    Add plotting recipes.
    Implementation is ugly, will need to be changed.

--- fitgrid/fitgrid.py
+++ fitgrid/fitgrid.py
@@ -1,8 +1,11 @@
 import numpy as np
 import pandas as pd
 from functools import lru_cache
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 from .errors import FitGridError
+from . import plots
 
 
 def values_are_series(temp):
@@ -177,3 +180,98 @@ class FitGrid:
     def __dir__(self):
 
         return dir(self.tester)
+
+    def plot_betas(self, channel=None, beta_name=None):
+
+        if (channel and beta_name) or (beta_name is None and channel is None):
+            raise NotImplementedError('Pass either channel or beta name.')
+
+        if channel:
+            plots.stripchart(self.params[channel])
+
+        if beta_name:
+            assert beta_name in self.betas_names
+            data = (self.params.swaplevel(axis=1)
+                               .sort_index(axis=1, level=0)
+                               .reindex(axis=1,
+                                        level=1,
+                                        labels=self.grid.columns))
+            plots.stripchart(data[beta_name])
+
+    def plot_adj_rsquared(self, by=None):
+
+        if by is None:
+            plt.figure(figsize=(16, 8))
+            sns.heatmap(self.rsquared_adj.T)
+        elif by == 'channels':
+            self.rsquared_adj.mean(axis=0).plot(kind='bar', figsize=(16, 8))
+        elif by == 'time':
+            self.rsquared_adj.mean(axis=1).plot(figsize=(16, 8))
+
+    def influential_epochs(self, top=20, within_channel=None):
+
+        influence = self.get_influence()
+
+        if within_channel is not None:
+            a = influence.cooks_distance[within_channel].drop(axis=0,
+                                                              index=1,
+                                                              level=1)
+            a.index = a.index.droplevel(1)
+            result = (a.mean(axis=0)
+                       .sort_values(ascending=False)
+                       .to_frame(name='average_Cooks_D'))
+        else:
+            a = influence.cooks_distance.drop(axis=0, index=1, level=1)
+            a.index = a.index.droplevel(1)
+            result = (a.mean(axis=1, level=1)
+                       .mean(axis=0)
+                       .sort_values(ascending=False)
+                       .to_frame(name='average_Cooks_D'))
+
+        return result.iloc[:top]
+
+    def plot_averaged_studentized_residuals(self, within_channel=None):
+        """
+        Parameters
+        ----------
+
+        within : str
+            channel name
+        """
+        influence = self.get_influence()
+
+        if within_channel is not None:
+            ar = (influence.resid_studentized_internal[within_channel]
+                           .mean(axis=0)
+                           .to_frame())
+        else:
+            ar = (influence.resid_studentized_internal.mean(axis=1, level=1)
+                                                      .mean(axis=0)
+                                                      .to_frame())
+        plt.figure(figsize=(16, 8))
+        sns.distplot(ar, bins=10)
+
+    def plot_averaged_absolute_standardized_residuals(self,
+                                                      within_channel=None):
+        """
+        Parameters
+        ----------
+
+        across : str
+            possible values are `epochs`, `time`, `channels`
+        """
+
+        influence = self.get_influence()
+
+        if within_channel is not None:
+            ar = (influence.resid_studentized_internal[within_channel]
+                           .abs()
+                           .mean(axis=0)
+                           .to_frame())
+        else:
+            ar = (influence.resid_studentized_internal
+                           .abs().mean(axis=1, level=1)
+                           .mean(axis=0)
+                           .to_frame())
+        plt.figure(figsize=(16, 8))
+        sns.distplot(ar, bins=10)
