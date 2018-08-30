c055fa824c4ad2eb7cba8dc732b3e92cfc17e545
Andrey Portnoy <aportnoy@ucsd.edu>
2018-08-30 16:26:37 -0700

    Improve io.epochs_from_hdf and add tests for io.
    Previously reading from hdf assumed no index, but presence of EPOCH_ID
    and TIME in the columns. Turns out, you can get a multiindexed dataframe
    by reading hdf files using pandas.
    
    So now we look for these variables in the index and the columns, and
    throw an error if we don't find them.

--- fitgrid/io.py
+++ fitgrid/io.py
@@ -1,14 +1,27 @@
 import pandas as pd
 from .epochs import Epochs
+from .errors import FitGridError
 from . import EPOCH_ID, TIME
 
 
 def epochs_from_hdf(hdf_filename):
     """Construct Epochs object from an HDF5 file containing an epochs table."""
-    df = (pd.read_hdf(hdf_filename)
-            .set_index([EPOCH_ID, TIME])
-            .sort_index())
-    return Epochs(df)
+
+    df = pd.read_hdf(hdf_filename)
+
+    # time and epoch id already present in index
+    if EPOCH_ID in df.index.names and TIME in df.index.names:
+        return Epochs(df)
+
+    # time and epoch id present in columns, set index
+    if EPOCH_ID in df.columns and TIME in df.columns:
+        df.set_index([EPOCH_ID, TIME], inplace=True)
+        return Epochs(df)
+
+    raise FitGridError(
+        f'Dataset has to contain {EPOCH_ID} and {TIME} as columns'
+        ' or indices.'
+    )
 
 
 def epochs_from_dataframe(dataframe):
---
+++ tests/test_io.py
@@ -0,0 +1,17 @@
+import fitgrid
+
+
+def test__epochs_from_hdf():
+    TEST_FILE = 'data/fake_epochs.h5'
+    fitgrid.epochs_from_hdf(TEST_FILE)
+
+
+def test__epochs_from_dataframe_good_data():
+
+    # should silently create an Epochs object from normally generated data
+    # this is a helper function, all further testing of Epochs creation is
+    # done in test_epochs.py
+    table = fitgrid.fake_data._generate(
+        n_epochs=10, n_samples=100, n_categories=2, n_channels=32
+    )
+    fitgrid.epochs_from_dataframe(table)
