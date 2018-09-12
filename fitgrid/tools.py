d0b37397c0c3e1b9d27f79df04f021a600e2e09b
Andrey Portnoy <aportnoy@ucsd.edu>
2018-09-11 21:16:43 -0700

    Make message on duplicate indices helpful: list dupes and locations.
    Previously the __repr__ of the index was dumped. Now we print a table of
    duplicate indices in EPOCH_ID and their locations. This closes #1.

---
+++ fitgrid/tools.py
@@ -0,0 +1,27 @@
+import numpy as np
+from collections import defaultdict
+
+
+def get_index_duplicates_table(df, level):
+    """Return a string table of duplicate index values and their locations."""
+
+    assert level in df.index.names
+
+    level_values = df.index.get_level_values(level)
+    dupe_mask = level_values.duplicated(keep=False)
+
+    dupes = level_values[dupe_mask]
+    dupe_indices = np.flatnonzero(dupe_mask)
+
+    dupe_dict = defaultdict(list)
+    for index, value in zip(dupe_indices, dupes):
+        dupe_dict[value].append(index)
+
+    # hardcoded padding
+    msg = f'Duplicates in index level {level}:\n\n'
+    msg += '{0:<20} {1}\n'.format(level, 'Locations')
+    for value, locations in sorted(dupe_dict.items()):
+        locations = (str(item) for item in locations)
+        msg += '{0:<20} {1}\n'.format(value, ', '.join(locations))
+
+    return msg
