138c88625d4f1d202a10b8722dfae870dd028d68
Andrey Portnoy <aportnoy@ucsd.edu>
2018-09-15 13:49:31 -0700

    Add matplotlib context manager for font size to stripchart.

--- fitgrid/plots.py
+++ fitgrid/plots.py
@@ -4,18 +4,17 @@ import matplotlib.pyplot as plt
 
 def stripchart(data, negative_up=True):
 
-    plt.rcParams.update({'font.size': 15})
+    with plt.rc_context({'font.size': 14}):
+        if isinstance(data, pd.Series):
+            data = data.unstack()
+        _, n = data.shape
+        fig, axes = plt.subplots(nrows=n, figsize=(16, n * 3), sharey=True)
+        data.plot(subplots=True, ax=axes)
 
-    if isinstance(data, pd.Series):
-        data = data.unstack()
-    _, n = data.shape
-    fig, axes = plt.subplots(nrows=n, figsize=(16, n * 3), sharey=True)
-    data.plot(subplots=True, ax=axes)
+        for ax in axes:
+            ax.set(xlabel='')
 
-    for ax in axes:
-        ax.set(xlabel='')
-
-    plt.tight_layout()
-    if negative_up:
-        plt.gca().invert_yaxis()
-    plt.show()
+        plt.tight_layout()
+        if negative_up:
+            plt.gca().invert_yaxis()
+        plt.show()
