d237912ded1272449c80a288b4b8f9de186c49c6
Andrey Portnoy <aportnoy@ucsd.edu>
2018-10-20 00:34:15 -0700

    Update setup.py with description, license type, url, exclude tests.

--- setup.py
+++ setup.py
@@ -10,12 +10,20 @@
 
 from setuptools import find_packages, setup
 
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
 setup(
     name='fitgrid',
     version='0.1.2',
     description='Mass multiple regression manager',
+    long_description=readme(),
+    long_description_content_type='text/markdown',
     author='Thomas P. Urbach, Andrey Portnoy',
     author_email='turbach@ucsd.edu, aportnoy@ucsd.edu',
-    packages=find_packages(),
+    url='https://github.com/kutaslab/fitgrid',
+    license='BSD-3-Clause',
+    packages=find_packages(exclude=['tests']),
     install_requires=['patsy', 'statsmodels', 'tqdm', 'tables'],
 )
