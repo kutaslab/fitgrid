e6576c6586845a72205e417bd2fc22681551c69d
Andrey Portnoy <aportnoy@ucsd.edu>
2018-11-30 16:04:03 -0800

    Add FitGrid saving and loading (grid.save and fitgrid.load_grid).
    FitGrid objects can now be saved to files and loaded from files. This is
    achieved by pickling a tuple containing grid._grid and
    grid._epoch_index. The highest protocol available in pickle is used. For
    Python 3.6 this means protocol 4. Loading is done by unpickling this
    tuple and creating a new FitGrid object using the constructor.

--- tests/test_io.py
+++ tests/test_io.py
@@ -17,3 +17,6 @@ def test__epochs_from_dataframe_good_data():
         n_epochs=10, n_samples=100, n_categories=2, n_channels=32
     )
     fitgrid.epochs_from_dataframe(table, channels)
+
+
+# fitgrid.load_grid is tested in test_fitgrid.py
