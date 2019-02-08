f6e1903dc3a4fb786bd0e6a43fe2834f12e5757c
Andrey Portnoy <aportnoy@ucsd.edu>
2019-02-07 16:20:04 -0800

    Release 0.2.1

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ def readme():
 
 setup(
     name='fitgrid',
-    version='0.2.0',
+    version='0.2.1',
     description='Mass multiple regression manager',
     long_description=readme(),
     long_description_content_type='text/markdown',
